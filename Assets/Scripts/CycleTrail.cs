7488fed9a8a3e59cede53bad00c1fc2dac5f505c
hectorborges <38707692+hectorborges@users.noreply.github.com>
2019-05-24 16:37:19 -0500

    Initial commit

---
+++ Assets/Scripts/CycleTrail.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CycleTrail : MonoBehaviour
+{
+    public TrailRenderer cycleTrail;
+    public LayerMask riderLayer;
+
+    //change to not run constantly
+
+    private void Update()
+    {
+        Vector3[] points = new Vector3[cycleTrail.positionCount];
+        cycleTrail.GetPositions(points);
+
+        if (points.Length <= 1) return;
+            
+        for(int i = 0; i < points.Length; i++)
+        {
+            if (i + 1 >= points.Length)
+                return;
+
+            RaycastHit hit;
+            if (Physics.Linecast(points[i], points[i + 1], out hit, riderLayer))
+            {
+                if(hit.transform != transform)
+                    print("hit target");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+    public float fudgeFactor; //max distance on y between controllers before triggering a turn.
+
+    public bool ignoreInput;
+
+    Vector3 leftControl;
+    Vector3 rightControl;
+    CycleTrail cycleTrail;
+
+    bool hasTurned;
+
+    private void Start()
+    {
+        cycleTrail = GetComponent<CycleTrail>();
+    }
+
+    private void Update()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+
+        if (ignoreInput) return;
+
+        OVRInput.Update();
+
+        leftControl = OVRInput.GetLocalControllerPosition(OVRInput.Controller.LTouch);
+        rightControl = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
+        print("LC: " + leftControl);
+        print("RC: " + rightControl);
+
+        //Find lowest control
+
+        float distance = rightControl.y - leftControl.y; //If Dist is positive we turn left, else go right.
+        int turnDirection = distance > 0 ? -1 : 1; //-1 = left, 1 = right
+
+        if (Mathf.Abs(distance) >= fudgeFactor && !hasTurned) //Check dist to see if passed fudge factor
+        {
+            hasTurned = true;
+            transform.rotation *= Quaternion.Euler(0, 90 * turnDirection, 0);
+        }
+        else if (Mathf.Abs(distance) < fudgeFactor && hasTurned)
+            hasTurned = false;
+    }
+}
